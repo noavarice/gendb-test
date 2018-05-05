cbe646dc909f305282df87809553bfefd404141d
noavarice <noavarice@gmail.com>
2018-05-05 23:42:55 +0300

    fix?

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,19 +1,23 @@
 package com;
 
 import com.gendb.Generator;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
-import javax.xml.bind.JAXBException;
 
 public class Main {
 
-  public static void main(String[] args) throws JAXBException {
+  public static void main(String[] args) throws IOException {
     final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     final Validator v = factory.getValidator();
     Generator g = new Generator(v);
     final InputStream is = ClassLoader.getSystemResourceAsStream("test.xml");
-    g.fromStream(is, null);
+    final File f = new File("/home/alexrazinkov/resources/test.txt");
+    f.createNewFile();
+    g.fromStream(is, new FileOutputStream(f));
   }
 }
