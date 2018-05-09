d9cae93fa9c7309a97c65aab561e99488e8d8ae9
noavarice <noavarice@gmail.com>
2018-05-09 14:54:08 +0300

    API changes

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,23 +1,19 @@
 package com;
 
 import com.gendb.Generator;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, URISyntaxException {
     final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     final Validator v = factory.getValidator();
-    Generator g = new Generator(v);
-    final InputStream is = ClassLoader.getSystemResourceAsStream("test.xml");
-    final File f = new File("/home/alexrazinkov/resources/test.txt");
-    f.createNewFile();
-    g.fromStream(is, new FileOutputStream(f));
+    Generator g = new Generator(Paths.get(ClassLoader.getSystemResource("test.xml").toURI()), v);
+    g.createScript(Paths.get("/home/alexrazinkov/resources/test.sql"), true);
   }
 }
